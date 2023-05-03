0e789f5b264d87909ff2df029b679100042bcb25
Alex <luckyleck29@gmail.com>
2023-05-02 22:24:37 -0400

    keyup creation

--- wordle/src/hooks/useWordle.js
+++ wordle/src/hooks/useWordle.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 
-function useWordle (solution) {
+function useWordle(solution) {
     const [turn, setTurn] = useState('')
     const [currentGuess, setCurrentGuess] = useState('')
     const [guesses, setGuesses] = useState([]) // each guess is an array
@@ -9,18 +9,31 @@ function useWordle (solution) {
     const [isCorrect, setIsCorrect] = useState(false)
 
     function formatGuess(guess) {
-        
+
     }
 
     function addNewGuess(guess) {
 
     }
 
-    function handleKeyup() {
-
+    function handleKeyup({ key }) {
+        if (key === 'Backspace') {
+            setCurrentGuess((prev) => {
+                return prev.slice(0, -1)
+            })
+            return
+        }
+        if (/^[A-Za-z]$/.test(key)) {
+            if (currentGuess.length < 5) {
+                setCurrentGuess((prev) => {
+                    return prev += key
+                })
+            }
+        }
+        console.log(key)
     }
 
-    return {turn, currentGuess, guesses, isCorrect, handleKeyup}
+    return { turn, currentGuess, guesses, isCorrect, handleKeyup }
 }
 
 export default useWordle
