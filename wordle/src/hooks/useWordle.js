70443be343437d7330a63a471b3639a2c2884126
Alex <luckyleck29@gmail.com>
2023-05-02 23:14:19 -0400

    add colors

--- wordle/src/hooks/useWordle.js
+++ wordle/src/hooks/useWordle.js
@@ -10,6 +10,28 @@ function useWordle(solution) {
 
     function formatGuess(guess) {
 
+        let solutionArray = [...solution] // turning string into array of individual letters
+        let formattedGuess = [...currentGuess].map((char) => {
+            return {letter: char, color: 'grey'}
+        })
+
+        //find greens
+        formattedGuess.forEach((letterObj, i) => {
+            if (solutionArray[i] === letterObj.letter) {
+                formattedGuess[i].color= 'green'
+                solutionArray[i] = null
+            }
+        })
+
+        //find yellows
+        formattedGuess.forEach((letterObj, i) => {
+            if (solutionArray.includes(letterObj.letter) && letterObj.color !== 'green') {
+                formattedGuess[i].color = 'yellow'
+                solutionArray[solutionArray.indexOf(letterObj.letter)] = null
+            }
+        })
+
+        return formattedGuess
     }
 
     function addNewGuess(guess) {
@@ -17,12 +39,34 @@ function useWordle(solution) {
     }
 
     function handleKeyup({ key }) {
+        if (key === 'Enter') {
+
+            if (turn > 5) {
+                console.log('you used all your guesses')
+                return
+            }
+
+            if (history.includes(currentGuess)) {
+                console.log('you\'ve already tried that word')
+                return
+            }
+
+            if (currentGuess.length !== 5) {
+                console.log('word must be 5 chars long')
+                return
+            }
+
+            const formatted = formatGuess()
+            console.log(formatted)
+        }
+
         if (key === 'Backspace') {
             setCurrentGuess((prev) => {
                 return prev.slice(0, -1)
             })
             return
         }
+
         if (/^[A-Za-z]$/.test(key)) {
             if (currentGuess.length < 5) {
                 setCurrentGuess((prev) => {
